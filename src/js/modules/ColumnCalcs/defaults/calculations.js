8f6371dae95b5605ed36d5477e97ce3e25280b9f
peter jenei <echo419p@gmail.com>
2024-06-24 09:33:02 +0200

    calculations fix for nan values

--- src/js/modules/ColumnCalcs/defaults/calculations.js
+++ src/js/modules/ColumnCalcs/defaults/calculations.js
@@ -1,56 +1,69 @@
 export default {
-	"avg":function(values, data, calcParams){
+	"avg": function (values, data, calcParams) {
 		var output = 0,
-		precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : 2;
+			precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : 2;
 
-		if(values.length){
-			output = values.reduce(function(sum, value){
+		if (values.length) {
+			const initialValue = 0;
+			var nullCount = 0;
+			output = values.reduce(function (sum, value) {
+
+				if (isNaN(value)) {
+					nullCount++;
+					return Number(sum);
+				}
+
 				return Number(sum) + Number(value);
-			});
 
-			output = output / values.length;
+			}, initialValue);
+
+			output = output / (values.length - nullCount);
 
 			output = precision !== false ? output.toFixed(precision) : output;
 		}
 
 		return parseFloat(output).toString();
 	},
-	"max":function(values, data, calcParams){
+	"max": function (values, data, calcParams) {
 		var output = null,
-		precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : false;
+			precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : false;
 
-		values.forEach(function(value){
+		values.forEach(function (value) {
 
 			value = Number(value);
 
-			if(value > output || output === null){
+			if (isNaN(value)) return;
+
+			if (value > output || output === null) {
 				output = value;
 			}
 		});
 
 		return output !== null ? (precision !== false ? output.toFixed(precision) : output) : "";
 	},
-	"min":function(values, data, calcParams){
+	"min": function (values, data, calcParams) {
 		var output = null,
-		precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : false;
+			precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : false;
 
-		values.forEach(function(value){
+		values.forEach(function (value) {
 
 			value = Number(value);
 
-			if(value < output || output === null){
+			if (isNaN(value)) return;
+
+			if (value < output || output === null) {
 				output = value;
 			}
 		});
 
 		return output !== null ? (precision !== false ? output.toFixed(precision) : output) : "";
 	},
-	"sum":function(values, data, calcParams){
+	"sum": function (values, data, calcParams) {
 		var output = 0,
-		precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : false;
+			precision = typeof calcParams.precision !== "undefined" ? calcParams.precision : false;
 
-		if(values.length){
-			values.forEach(function(value){
+		if (values.length) {
+			values.forEach(function (value) {
 				value = Number(value);
 
 				output += !isNaN(value) ? Number(value) : 0;
@@ -59,31 +72,31 @@ export default {
 
 		return precision !== false ? output.toFixed(precision) : output;
 	},
-	"concat":function(values, data, calcParams){
+	"concat": function (values, data, calcParams) {
 		var output = 0;
 
-		if(values.length){
-			output = values.reduce(function(sum, value){
+		if (values.length) {
+			output = values.reduce(function (sum, value) {
 				return String(sum) + String(value);
 			});
 		}
 
 		return output;
 	},
-	"count":function(values, data, calcParams){
+	"count": function (values, data, calcParams) {
 		var output = 0;
 
-		if(values.length){
-			values.forEach(function(value){
-				if(value){
-					output ++;
+		if (values.length) {
+			values.forEach(function (value) {
+				if (value) {
+					output++;
 				}
 			});
 		}
 
 		return output;
 	},
-	"unique":function(values, data, calcParams){
+	"unique": function (values, data, calcParams) {
 		var unique = values.filter((value, index) => {
 			return (values || value === 0) && values.indexOf(value) === index;
 		});
