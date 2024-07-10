f986d2f7eedc096c93f866602f72dd7747cb85fc
echo419 <echo419p@gmail.com>
2024-07-10 16:00:03 +0200

    import working

--- src/js/modules/Import/defaults/importers/xlsx.js
+++ src/js/modules/Import/defaults/importers/xlsx.js
@@ -2,7 +2,9 @@ export default function (input, floop) {
 
 	XLSX = document.XLSX;
 
-	var workbook2 = XLSX.read(input, { cellDates: true });
+	var workbook2 = XLSX.read(input, {
+		cellDates: true
+	});
 	var sheet = workbook2.Sheets[workbook2.SheetNames[0]];
 
 	var response = XLSX.utils.sheet_to_json(sheet, {});
