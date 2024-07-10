0ef9eb17738611e18d8d5376b5f0d8b40151a127
echo419 <echo419p@gmail.com>
2024-07-10 15:35:41 +0200

    date import progress

--- src/js/modules/Import/defaults/importers/xlsx.js
+++ src/js/modules/Import/defaults/importers/xlsx.js
@@ -2,15 +2,12 @@ export default function (input, floop) {
 
 	XLSX = document.XLSX;
 
-	var workbook2 = XLSX.read(input);
+	var workbook2 = XLSX.read(input, { cellDates: true });
 	var sheet = workbook2.Sheets[workbook2.SheetNames[0]];
-
-
-
 
 	var response = XLSX.utils.sheet_to_json(sheet, {});
 
-
+	console.warn("tabulator import -> ", response);
 
 	return response;
 }
